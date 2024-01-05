49290e624b1f428138b36776fb15b0af26a426eb
neo <lanqing.ji@foxmail.com>
2024-01-05 21:53:33 +0800

    1. Delete main.rs 2. Add the test in rsa.rs

--- src/crypto/rsa.rs
+++ src/crypto/rsa.rs
@@ -1,30 +1,26 @@
-use num_bigint::{BigUint, BigInt, ToBigInt};
+use num_bigint::{BigUint, ToBigInt};
 use num_integer::Integer;
-use num_traits::cast::ToPrimitive;
+
 #[cfg(test)]
 mod rsa_tests {
     use super::*;
     #[test]
     fn rsa_test() {
-        //let plain_text: String = String::from("SUNYSUNYSUNYSUNY");
-        //let rsa_key = String::from("abcdabcdabcdabcd").into_bytes();
-        //let n1 = BigNum::new(String::from("106697219132480173106064317148705638676529121742557567770857687729397446898790451577487723991083173010242416863238099716044775658681981821407922722052778958942891831033512463262741053961681512908218003840408526915629689432111480588966800949428079015682624591636010678691927285321708935076221951173426894836169"));
-        //let n2 = BigNum::new(String::from("144819424465842307806353672547344125290716753535239658417883828941232509622838692761917211806963011168822281666033695157426515864265527046213326145174398018859056439431422867957079149967592078894410082695714160599647180947207504108618794637872261572262805565517756922288320779308895819726074229154002310375209"));
+        let r = Rsa::new();
+        let msg = String::from("SUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNY");
+        println!("plaintext: {:?}\n", msg.as_bytes());
+        let ciphertext = r.enc(&msg.as_bytes());
+        let plaintext = r.dec(&ciphertext);
 
-        //let r = Rsa::new(n1 ,n2);
-        //println!("Rsa: {:?}", r);
-
-
-        //assert_eq!(
-        //     rsa_enc(&plain_text, &aes_key, AesType::AES128),
-        //     String::from("30e1afaf9c36e4814f2abfd05c76cf12")
-        //);
+        assert_eq!(
+             msg.as_bytes(),
+             plaintext,
+        );
     }
 }
 
 #[derive(Debug)]
 struct Key(BigUint, BigUint);
-
 
 #[derive(Debug)]
 pub struct Rsa {
@@ -32,12 +28,11 @@ pub struct Rsa {
     pub_key: Key,
 }
 
-
 impl Rsa {
 
     //fn gen_key(p: BigNum, q: BigNum) -> Rsa {
 
-   //}
+    //}
 
     pub fn new() -> Rsa{
         let p: BigUint = BigUint::parse_bytes(b"106697219132480173106064317148705638676529121742557567770857687729397446898790451577487723991083173010242416863238099716044775658681981821407922722052778958942891831033512463262741053961681512908218003840408526915629689432111480588966800949428079015682624591636010678691927285321708935076221951173426894836169", 10).unwrap();
@@ -48,6 +43,7 @@ impl Rsa {
         let fi = (p.clone() - 1 as u8)*(q.clone() - 1 as u8); 
 
         let res_gcd_ext = e.to_bigint().expect("covert to bigint fail!").extended_gcd(&fi.to_bigint().expect("covert to bigint fail!"));
+
         let mut d = res_gcd_ext.x;
         if d < 0.to_bigint().unwrap() {
             d = d + fi.to_bigint().unwrap();
@@ -89,5 +85,4 @@ impl Rsa {
         plain_text
     }
 }
-
 
