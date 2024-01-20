caf774c929984e9912c4404636b00dce34d9a6c5
neo <lanqing.ji@foxmail.com>
2024-01-20 20:00:12 +0800

    1. Change u128 to Vec<u8> 2. Previous issue still exsits (the CMAC of msg longer than 16 bytes is different from the result from Openssl)

--- src/cmac.rs
+++ src/cmac.rs
@@ -5,9 +5,11 @@ mod cmac_tests {
     use super::*;
     #[test]
     fn cmac_aes_ecb_128_test() {
-        let plaintext: Vec<u8> = String::from("SUNYSUNYSUNYSUNYJILQJILQJILQJILQ").into_bytes();
+        // let plaintext: Vec<u8> = String::from("SUNYSUNYSUNYSUNYJILQJILQJILQJILQ").into_bytes();
+        let plaintext: Vec<u8> = String::from("SUNYSUNYSUNYSUNYSUNYSUNYSUNYSUNY").into_bytes();
         // let plaintext: Vec<u8> = String::from("SUNYSUNYSUNYSUNY").into_bytes();
         // let plaintext: Vec<u8> = String::from("SUNY").into_bytes();
+        // let plaintext: Vec<u8> = String::from("SUNYSUNYSUNYSUNYJILQ").into_bytes();
         let key: [u8; 16] = [
             0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02, 0x03, 0x04, 0x01, 0x02,
             0x03, 0x04,
@@ -26,29 +28,54 @@ mod cmac_tests {
     }
 }
 
+fn bytes_xor(arr1: &[u8], arr2: &[u8]) -> Vec<u8> {
+    let mut output: Vec<u8> = arr1.to_vec();
+    for i in 0..arr1.len() {
+        output[i] = arr1[i] ^ arr2[i];
+    }
+    output
+}
+
+fn bytes_left_shfit(input: &[u8]) -> Vec<u8> {
+    let mut output: Vec<u8> = input.to_vec();
+    let mut overflow: u8 = 0;
+    let mut _temp: u8 = 0;
+
+    for i in (0..input.len()).rev() {
+        _temp = input[i] << 1;
+        _temp |= overflow;
+        overflow = if input[i] & 0x80 == 0x80 { 0x01 } else { 0x00 };
+        output[i] = _temp;
+    }
+    output
+}
+
 pub fn cmac(plaintext: &[u8], key: &[u8], aes_type: &AesType) -> Vec<u8> {
     let l: Vec<u8> = aes_ecb_enc(&[0; 16], key, aes_type);
 
-    let l_u128: u128 = u128::from_be_bytes(l.try_into().unwrap());
+    let const_rb: Vec<u8> = vec![
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x87,
+    ];
 
-    let k1: u128;
-
-    if l_u128 & 0x80000000000000000000000000000000 == 0 {
-        k1 = l_u128 << 1;
+    let _k1: Vec<u8> = if l[0] & 0x80 == 0x00 {
+        bytes_left_shfit(&l)
     } else {
-        k1 = (l_u128 << 1) ^ 0x0000000000000000000087;
-    }
+        bytes_xor(&bytes_left_shfit(&l), &const_rb)
+    };
 
-    let k2: u128;
-    if k1 & 0x80000000000000000000000000000000 == 0 {
-        k2 = k1 << 1;
+    let _k2: Vec<u8> = if l[0] & 0x80 == 0x00 {
+        bytes_left_shfit(&_k1)
     } else {
-        k2 = (k1 << 1) ^ 0x0000000000000000000087;
-    }
+        bytes_xor(&bytes_left_shfit(&_k1), &const_rb)
+    };
 
-    let mut ciphered_block: u128 = 0;
-    let mut res: Vec<u8> = Vec::new();
+    let mut ciphered_block: Vec<u8> = vec![
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00,
+    ];
 
+    let mut mac: Vec<u8> = Vec::new();
     let blocks = plaintext.chunks(16);
     let blocks_num: usize = blocks.len();
     let bytes_num: usize = match aes_type {
@@ -58,19 +85,12 @@ pub fn cmac(plaintext: &[u8], key: &[u8], aes_type: &AesType) -> Vec<u8> {
     };
 
     for (i, block) in blocks.into_iter().enumerate() {
+        let mut _m: Vec<u8> = Vec::new();
         if i < blocks_num - 1 {
-            let m: u128;
-
-            m = ciphered_block ^ u128::from_be_bytes(block.try_into().unwrap());
-
-            ciphered_block = u128::from_be_bytes(
-                aes_ecb_enc(&m.to_be_bytes().to_vec(), key, aes_type)
-                    .try_into()
-                    .unwrap(),
-            );
+            _m = bytes_xor(&ciphered_block, block);
+            ciphered_block = aes_ecb_enc(&_m, key, aes_type);
         } else {
             let mut last_block: Vec<u8> = block.to_vec();
-            let m: u128;
 
             if block.len() < 16 {
                 //in case that it's not a complete block (128 bits)
@@ -79,14 +99,14 @@ pub fn cmac(plaintext: &[u8], key: &[u8], aes_type: &AesType) -> Vec<u8> {
                 for _ in 0..(bytes_to_pad - 1) {
                     last_block.push(0x00);
                 }
-                m = ciphered_block ^ u128::from_be_bytes(last_block.try_into().unwrap()) ^ k2;
+                _m = bytes_xor(&bytes_xor(&_k2, &last_block), &ciphered_block);
             } else {
+                println!("ciphered_block: {:?}", &ciphered_block);
                 //in case that it's a complete block (128 bits)
-                m = ciphered_block ^ u128::from_be_bytes(last_block.try_into().unwrap()) ^ k1;
+                _m = bytes_xor(&bytes_xor(&_k1, &last_block), &ciphered_block);
             }
-
-            res = aes_ecb_enc(&m.to_be_bytes().to_vec(), key, aes_type);
+            mac = aes_ecb_enc(&_m, key, aes_type);
         }
     }
-    res
+    mac
 }
